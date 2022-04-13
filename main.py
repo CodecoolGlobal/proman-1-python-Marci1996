9a67d7906f4ce78feb334d9c0b1e93976cf9ab89
SzervetG <90453505+SzervetG@users.noreply.github.com>
2022-04-13 09:36:23 +0200

    create login and register functions

--- main.py
+++ main.py
@@ -1,19 +1,95 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session, redirect, request
 from dotenv import load_dotenv
 from util import json_response
 import mimetypes
 import queries
+import bcrypt
 
 mimetypes.add_type('application/javascript', '.js')
 app = Flask(__name__)
 load_dotenv()
+app.secret_key = 'nellyqueen'
+
+
+def is_logged_in():
+    if 'username' in session:
+        return True
+    else:
+        return False
+
+
+def hash_password(plain_text_password):
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
+
+@app.route('/register')
+def register():
+    if is_logged_in():
+        return redirect(url_for('index'))
+    else:
+        return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    all_username = queries.get_all_username()
+    if username in all_username:
+        return redirect(url_for('register'))
+    else:
+        hashed_password = hash_password(password)
+        queries.add_new_user(username, hashed_password)
+        return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    if is_logged_in():
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    all_username = queries.get_all_username()
+    if username in all_username:
+        user_data = queries.get_all_user_data(username)
+        if verify_password(password, user_data[0]['password']):
+            session['username'] = username
+            session['id'] = user_data[0]['id']
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    if is_logged_in():
+        session.clear()
+        return redirect(url_for('main_page'))
+    else:
+        return redirect(url_for('login_get'))
+
 
 @app.route("/")
 def index():
     """
     This is a one-pager which shows all the boards and cards
     """
-    return render_template('index.html')
+    logged_in = is_logged_in()
+    return render_template('index.html', logged_in=logged_in)
 
 
 @app.route("/api/boards")
